724eb3c470391df21fb93b7bcc5191ed9a942ec7
sahilkakwani9 <sahkakwani@gmail.com>
2022-04-15 01:06:38 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Hero from './components/Hero'
 import Navbar from './components/Navbar';
+import About from './components/About';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar/>
-      <Hero/>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from "react-router-dom";
+
 
 function Navbar() {
   return (
     <div className='nav-wrap'>
           <nav>
-            <li><a href="">Home</a></li>
-            <li><a href="">Projects</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Contact</a></li>
+            <li><a><Link to="/">Home</Link></a></li>
+            <li><a href=""><Link to="/projects">Projects</Link></a></li>
+            <li><a href=""><Link to="/about">About</Link></a></li>
+            <li><a href=""><Link to="/contact">Contact</Link></a></li>
           </nav>
     </div>
   )
