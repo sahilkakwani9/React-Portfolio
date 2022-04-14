afd2ed78881446f8b015e406b4811481682317c6
sahilkakwani9 <sahkakwani@gmail.com>
2022-04-14 21:09:24 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Hero from './components/Hero'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Hero/>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='nav-wrap'>
+          <nav>
+            <li><a href="">Home</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+          </nav>
+    </div>
+  )
+}
+
+export default Navbar
