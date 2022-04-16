54789bb55c1e5fcf9b96dc6c6f24f6728239c790
sahilkakwani9 <sahkakwani@gmail.com>
2022-04-16 13:27:39 +0530

    about intro

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './About.css'
 
 function About() {
   return (
-    <div>About</div>
+    <>
+      <div className="about-container">
+        <div className="intro">
+          <h1>intro.</h1>
+          <div className="intro-content">
+            <div className="img-wrap">
+              <img src="/sahil-headshot.jpg" alt="headshot" />
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Nisi vitae suscipit tellus mauris a. Orci nulla pellentesque dignissim enim. Ipsum dolor sit amet consectetur. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. Consectetur adipiscing elit ut aliquam purus sit amet luctus. Tortor id aliquet lectus proin nibh nisl. Iaculis urna id volutpat lacus laoreet non curabitur.</p>
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
