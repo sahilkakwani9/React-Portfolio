739c67682b731de0f50c199f432572ac1977d4fd
sahilkakwani9 <sahkakwani@gmail.com>
2022-04-18 20:33:04 +0530

    Added skills btn

--- src/components/About.js
+++ src/components/About.js
@@ -27,8 +27,8 @@ function About() {
           </div>
 
         </div>
-        <motion.div animate={{ y: motionList}} transition={{ ease: "easeOut", duration: 1 }} className='up'>
-          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>
+        <motion.div animate={{ y: [-10,10,-10]}} transition={{ ease: "easeOut", duration: 1, repeat: Infinity }} className='up'>
+          <svg stroke="currentColor" fill="#551a8b" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>
           <div className='txt-wrap'>
             <p>Skills</p>
           </div>
