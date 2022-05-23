42b93495a87428bc65b6e55b5b649e686e9e9677
sahilkakwani9 <sahkakwani@gmail.com>
2022-05-23 20:09:55 +0530

    minor-changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/projects">Projects</Link></li>
             <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact" class="phone-none">Contact</Link></li>
+            {/* <li><Link to="/contact" class="phone-none">Contact</Link></li> */}
           </nav>
     </div>
   )
