c8a6ef7205b0668a68bdcac794dd447e31de6c6a
sahilkakwani9 <sahkakwani@gmail.com>
2022-07-08 19:59:47 +0530

    image problem

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ function Main() {
           <div className="subtitle">Deep Thinker and Web Developer</div>
         </div>
         <div className='headshot-img'>
-          <img src="sahil-headshot-c.JPG" alt="" />
+          <img src="./sahil-headshot-c.jpg" alt="" />
           <div className='img-txt'>I am a Front-end web developer based out of gujarat, India. A keen learner and believer of consistent growth. I aspire to work at small and growing startups where i can employ my skills.</div>
         </div>
       </div>
