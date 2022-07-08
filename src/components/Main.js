c5ae1dc4caf812d0f8adc48efc04bf7a83bd25d3
sahilkakwani9 <sahkakwani@gmail.com>
2022-07-09 01:53:48 +0530

    added skill slider

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import './Main.css'
 function Main() {
   return (
     <div className='main-container'>
+      <div className='text-container'>
       <div className="bio">
         <div className='data'>
           <h1 className='author'>Sahil Kakwani</h1>
@@ -14,6 +15,38 @@ function Main() {
           <div className='img-txt'>I am a Front-end web developer based out of gujarat, India. A keen learner and believer of consistent growth. I aspire to work at small and growing startups where i can employ my skills.</div>
         </div>
       </div>
+      <div className="skills-container">
+        <h3>Skills</h3>
+        <div className='skills-slider'>
+          <div className='list'>
+            <img src="./html5.svg" alt="html" />
+            {/* <p>HTML</p> */}
+          </div>
+          <div className='list'>
+            <img src="./css3.svg" alt="css" />
+          </div>
+          <div className='list'>
+            <img src="./javascript.svg" alt="js" />
+          </div>
+          <div className='list'>
+            <img src="./nodejs.svg" alt="node" />
+          </div>
+          <div className='list'>
+            <img src="./react.svg" alt="react" />
+          </div>
+          <div className='list'>
+            <img src="./python.svg" alt="python" />
+          </div>
+          <div className='list'>
+            <img src="./git.svg" alt="git" />
+          </div>
+          <div className='list'>
+            <img className='express' src="./express.png" alt="express" />
+          </div>
+        </div>
+      </div>
+      </div>
+      
     </div>
   )
 }
