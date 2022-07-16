4e0960f58399662cf7852264e4597d3dc3efd0d3
sahilkakwani9 <sahkakwani@gmail.com>
2022-07-17 00:17:47 +0530

    Footer done

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ function Main() {
           <div className="subtitle">Deep Thinker and Web Developer</div>
         </div>
         <div className="skills-container">
-        <h3>Skills</h3>
+        <h3>Skills</h3> 
         <div className='skills-slider'>
           <div className='list'>
             <img src="./html5.svg" alt="html" />
@@ -52,7 +52,7 @@ function Main() {
         </div>  
     </div>
     <div className='tweets-container'>
-      <h2>Recent Tweets</h2>
+      <h2>Tweets</h2>
       <div className="tweets">
       <TwitterTweetEmbed className='tweet' tweetId={'1545784495798112256'}/>
       <TwitterTweetEmbed className='tweet' tweetId={'1545468748550766595'}/>
@@ -70,6 +70,13 @@ function Main() {
     <div className='footer'>
       <div className='knowmore'>
         <h2>Want to know more?</h2>
+        <div className='then'>
+          <h2>Then</h2>
+          <a href="mailto:sahkakwani@gmail.com" class="rainbow-button" alt="Button"></a>
+        </div>
+        
+          {/* <button><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" role="img" class="icon "><path d="m12 13.595c-.715 0-1.43-.153-2.095-.46l-9.905-4.572v11.437c0 1.105.895 2 2 2h20c1.105 0 2-.895 2-2v-11.437l-9.905 4.572c-.665.307-1.38.46-2.095.46zm10-11.595h-20c-1.105 0-2 .895-2 2v2.36l10.743 4.958c.799.368 1.716.369 2.515 0l10.742-4.958v-2.36c0-1.105-.895-2-2-2z"></path></svg><p>Mail me</p></button> */}
+        
       </div>
     </div>
     </div>
