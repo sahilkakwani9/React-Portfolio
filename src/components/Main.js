94c5ce66155c2b27b336bb84c47b4c2305b91301
sahilkakwani9 <sahkakwani@gmail.com>
2022-11-13 17:25:36 +0530

    fix: design

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,7 +24,7 @@ function Main() {
       <div className="bio">
         <div className='data'>
           <h1 className='author'>Sahil Kakwani</h1>
-          <div className="subtitle"><span className='wavy'>Deep</span> <span className='wavy'>Thinker</span> and <span className='wavy'>Web</span> <span className='wavy'>Developer</span></div>
+          <div className="subtitle"><span className='wavy'>Passionate</span> Learner, <span className='wavy'>Deep</span> Thinker and <span className='wavy'>Web</span> Developer</div>
         </div>
         <div className="skills-container">
         <h3>Skills</h3> 
@@ -106,8 +106,6 @@ function Main() {
           <a href="mailto:sahkakwani@gmail.com" class="rainbow-button" alt="Button"></a>
         </div>
         
-          {/* <button><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" role="img" class="icon "><path d="m12 13.595c-.715 0-1.43-.153-2.095-.46l-9.905-4.572v11.437c0 1.105.895 2 2 2h20c1.105 0 2-.895 2-2v-11.437l-9.905 4.572c-.665.307-1.38.46-2.095.46zm10-11.595h-20c-1.105 0-2 .895-2 2v2.36l10.743 4.958c.799.368 1.716.369 2.515 0l10.742-4.958v-2.36c0-1.105-.895-2-2-2z"></path></svg><p>Mail me</p></button> */}
-        
       </div>
       <div className='end'>
         <div className='line'>
@@ -117,7 +115,6 @@ function Main() {
         </div>
         <div className='text'>
           <p>&copy; 2022,&nbsp;<a href="https://github.com/sahilkakwani9">Sahil Kakwani</a></p>
-          {/* <p>Inspired from: <a href="https://addyosmani.com/">Addy Osmani</a></p> */}
         </div>
       </div>
     </div>
