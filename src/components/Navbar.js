694b9e99b51b73f3cb639994567bab1c01aa2029
sahilkakwani9 <sahkakwani@gmail.com>
2022-11-19 16:28:22 +0530

    fea: fix: tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Navbar.css'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 
 
 function Navbar() {
+  const page = useLocation();
   return (
     <div className='nav-wrap'>
           <nav>
-          <li><Link to="/">Home</Link></li>
-            <li><a href="/Resume.pdf" download>Resume</a></li>
-            <li><Link to="/projects">Projects</Link></li>
+          <li aria-label={`${page.pathname == "/"? "page":undefined}`}><Link to="/">Home</Link></li>
+            <li aria-label={`${page.pathname == "/resume"? "page":undefined}`}><a href="/Resume.pdf" download>Resume</a></li>
+            <li aria-label={`${page.pathname == "/projects"? "page":undefined}`}><Link to="/projects">Projects</Link></li>
             {/* <li><Link to="/contact" class="phone-none">Contact</Link></li> */}
           </nav>
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,73 +1,72 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Projects.css'
 import projects from "../Projects.json"
 import { useEffect } from 'react';
 
 function Projects() {
     const [filter, setFilter] = useState("all");
-    const [allActive, setAllActive] = useState("blue");
-    const [collabActive, setCollabActive] = useState("white")
-  return (
-    <div className='project-container'>
-        <div className="project-wrapper">
-            {/* <h1>Work</h1> */}
-            <div className='window'>
-                <button  onClick={()=>{setFilter("all");setAllActive("blue");setCollabActive("white")}} className={`all toggle ${allActive}`}>All</button>
-                {/* <button  onClick={() => { alert("hello world")}} >Sahil</button> */}
-                <button className={`collab toggle ${collabActive}`} onClick={()=>{setFilter("collab");setAllActive("white");setCollabActive("blue")}}>Colloborated</button>
-            </div>
-            <div className="card-wrap">
-               
-                <div className='card-box'>
-                    <div className='card-skeleton'>
-                    <img src='./disney_hero.webp' alt='disney' />
-                    <div className='content'>
-                    <h2>EthIndia Builder House</h2>
-                    <p>A replica of Disney, made with React,Styled Components and Firebase</p>
-                    <button>Watch Live</button>
-                    </div>
+    const [active, setActive] = useState("all");
+    return (
+        <div className='project-container'>
+            <div className="project-wrapper">
+                <div className='window'>
+                    <div className='tab'>
+                        <button onClick={() => { setFilter("all"); setActive("all"); }} className={`all toggle ${active}`}>All</button>
+                        <button className={`collab toggle ${active}`} onClick={() => { setFilter("collab"); setActive("collab"); }}>Colloborated</button>
+                        <div className={`blue ${active}`}></div>
                     </div>
                 </div>
-                {/* {projects && projects.map((e,i)=>{
-                    if ( filter === "collab" ){
-                        if (e.colloborated === true) {
+                <div className="card-wrap">
+
+
+                    {projects && projects.map((e, i) => {
+                        if (filter === "collab") {
+                            if (e.colloborated === true) {
+                                return (
+                                    // <div className={e.class}>
+                                    //     <img src={e.src} alt="project" />
+                                    //     <div className="body">
+                                    //         <h3 className="project-name">{e.name}</h3>
+                                    //         <p>{e.Description}</p>
+                                    //         <p className='won'>{e.won}</p>
+                                    //         <div className="btn-wrap">
+                                    //             <button><a className='btn-txt' href={e.href}>Watch Live</a></button>
+                                    //         </div>
+                                    //     </div>
+                                    // </div>
+                                    <div className='card-box'>
+                                    <div className='card-skeleton'>
+                                        <img src={e.src} alt='disney' />
+                                        <div className='content'>
+                                            <h2>{e.name}</h2>
+                                            <p>{e.Description}</p>
+                                            <a href={e.href}><button>Watch Live</button></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                )
+                            }
+
+                        }
+                        else {
                             return (
-                                <div className={e.class}>
-                                    <img src={e.src} alt="project" />
-                                    <div className="body">
-                                        <h3 className="project-name">{e.name}</h3>
-                                        <p>{e.Description}</p>
-                                        <p className='won'>{e.won}</p>
-                                        <div className="btn-wrap">
-                                            <button><a className='btn-txt' href={e.href}>Watch Live</a></button>
+                                <div className='card-box'>
+                                    <div className='card-skeleton'>
+                                        <img src={e.src} alt='disney' />
+                                        <div className='content'>
+                                            <h2>{e.name}</h2>
+                                            <p>{e.Description}</p>
+                                            <a href={e.href}><button>Watch Live</button></a>
                                         </div>
                                     </div>
                                 </div>
                             )
                         }
-                        
-                    }
-                    else {
-                        return (
-                            <div className={e.class}>
-                                <img src={e.src} alt="project" />
-                                <div className="body">
-                                    <h3 className="project-name">{e.name}</h3>
-                                    <p>{e.Description}</p>
-                                    <p className='won'>{e.won}</p>
-                                    <div className="btn-wrap">
-                                        <button><a className='btn-txt' href={e.href}>Watch Live</a></button>
-                                        <button><a className='btn-txt' href={e.href}>Tech Stack</a></button>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    }
-                })} */}
+                    })}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Projects
