508529ead1a8da0db975740dd0cd92e3c5763b36
sahilkakwani9 <sahkakwani@gmail.com>
2022-11-27 01:47:00 +0530

    fea: framer

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,40 +1,35 @@
 import React, { useState } from 'react'
 import './Projects.css'
 import projects from "../Projects.json"
-import { useEffect } from 'react';
+import { motion } from 'framer-motion';
+
 
 function Projects() {
-    const [filter, setFilter] = useState(true);
+    const [filter, setFilter] = useState("all");
     const [active, setActive] = useState("all");
+    
     return (
         <div className='project-container'>
             <div className="project-wrapper">
                 <div className='window'>
                     <div className='tab'>
-                        <button onClick={() => { setFilter(false); setActive("all"); }} className={`toggle ${filter?'':'white'}`}>All</button>
-                        <button className={`toggle ${filter?'white':''}`} onClick={() => { setFilter(true); setActive("collab"); }}>Colloborated</button>
+                        <button onClick={() => { setFilter("all"); setActive("all"); }} className={`toggle ${filter?'':'white'}`}>All</button>
+                        <button className={`toggle ${filter?'white':''}`} onClick={() => { setFilter("collab"); setActive("collab"); }}>Colloborated</button>
                         <div className={`blue ${active}`}></div>
                     </div>
                 </div>
-                <div className="card-wrap">
+                <motion.div className="card-wrap" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
 
 
-                    {projects && projects.map((e, i) => {
+                    {projects && projects.map((e, id) => {
                         if (filter === "collab") {
                             if (e.colloborated === true) {
                                 return (
-                                    // <div className={e.class}>
-                                    //     <img src={e.src} alt="project" />
-                                    //     <div className="body">
-                                    //         <h3 className="project-name">{e.name}</h3>
-                                    //         <p>{e.Description}</p>
-                                    //         <p className='won'>{e.won}</p>
-                                    //         <div className="btn-wrap">
-                                    //             <button><a className='btn-txt' href={e.href}>Watch Live</a></button>
-                                    //         </div>
-                                    //     </div>
-                                    // </div>
-                                    <div className='card-box'>
+                                    <motion.div initial={{ opacity: 0, y: 50 }} whileInView={{ transition: {
+                                        type: "spring",
+                                        bounce: 0.4,
+                                        duration: 0.8
+                                      }, opacity: 1}} id={id} key={id} className='card-box'>
                                     <div className='card-skeleton'>
                                         <img src={e.src} alt='disney' />
                                         <div className='content'>
@@ -43,14 +38,14 @@ function Projects() {
                                             <a href={e.href}><button>Watch Live</button></a>
                                         </div>
                                     </div>
-                                </div>
+                                </motion.div>
                                 )
                             }
 
                         }
                         else {
                             return (
-                                <div className='card-box'>
+                                <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} id={id} key={id} className='card-box'>
                                     <div className='card-skeleton'>
                                         <img src={e.src} alt='disney' />
                                         <div className='content'>
@@ -59,11 +54,11 @@ function Projects() {
                                             <a href={e.href}><button>Watch Live</button></a>
                                         </div>
                                     </div>
-                                </div>
+                                </motion.div>
                             )
                         }
                     })}
-                </div>
+                </motion.div>
             </div>
         </div>
     )
