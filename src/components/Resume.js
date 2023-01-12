6e4e715344a8aadd92a6286470440d165d21e482
sahilkakwani9 <sahkakwani@gmail.com>
2023-01-12 14:16:37 +0530

    fea: onload resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,19 +5,27 @@ import './Resume.css'
 function Resume() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
+  const [pageLoaded,setPageLoaded] = useState(false);
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
+    console.log(numPages);
+
+    if (numPages){
+      setPageLoaded(true);
+      console.log(pageLoaded);
+    }
   };
 
   return (
-    <div className='resume'>
-      <div className='pdf-container'>
+      <div className='resume'>
+      <div className={pageLoaded?'pdf-container':''}>
 
         <Document
           file="/Resume.pdf"
           onLoadSuccess={onDocumentLoadSuccess}
           onLoadError={console.error}
+          loading={<h1 color='#ffffff'>loading</h1>}
         >
           <Page pageNumber={pageNumber} />
         </Document>
