34635ba27b883c9b99669b13e50ba48c3480df27
sahilkakwani9 <sahkakwani@gmail.com>
2023-01-12 13:56:57 +0530

    fix: final fix

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,25 +1,27 @@
 import { useState } from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { Document, Page, pdfjs } from 'react-pdf';
 import './Resume.css'
 
 function Resume() {
   const [numPages, setNumPages] = useState(null);
-	const [pageNumber, setPageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
   const onDocumentLoadSuccess = ({ numPages }) => {
-		setNumPages(numPages);
-	};
+    setNumPages(numPages);
+  };
 
   return (
     <div className='resume'>
       <div className='pdf-container'>
 
-<Document
-  file="/Resume.pdf"
-  onLoadSuccess={onDocumentLoadSuccess}
->
-  <Page pageNumber={pageNumber} />
-</Document>
-</div>
+        <Document
+          file="/Resume.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+          onLoadError={console.error}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </div>
     </div>
   )
 }
